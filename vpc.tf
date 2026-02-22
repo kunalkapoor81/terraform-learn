3b79b7808ba299d634da44cfa9cea2dfed6a13f9
Kunal Kapoor <kksworld@gmail.com>
2026-02-22 14:54:29 -0800

    Provision an EKS cluster

---
+++ vpc.tf
@@ -0,0 +1,43 @@
+provider "aws" {
+  region = "us-west-1"
+}
+
+variable vpc_cidr_block {}
+variable private_subnet_cidr_block {}
+variable public_subnet_cidr_block {}
+
+
+data "aws_availability_zones" "azs"{}
+    
+module "myapp-vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "6.6.0"
+
+  name = "myapp-vpc"
+  cidr = var.vpc_cidr_block
+  azs = data.aws_availability_zones.azs.names
+  private_subnets = var.private_subnet_cidr_block
+  public_subnets =  var.public_subnet_cidr_block
+
+  enable_nat_gateway = true
+  single_nat_gateway  = true
+  enable_dns_hostnames = true   
+
+  tags = {
+    "kubernetes.io/cluster/myapp-eks-cluster" = "shared"
+  }
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/myapp-eks-cluster" = "shared"
+    "kubernetes.io/role/elb" = 1
+
+
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/myapp-eks-cluster" = "shared"
+    "kubernetes.io/role/internal-elb" = 1
+
+  }
+
+}
